ed1ff8e7e905ac28696ca366fac214128d74317d
Kevin Lezcano <145628265+KX690@users.noreply.github.com>
2024-11-12 09:37:45 -0300

    Update cursosModels.js

--- src/models/cursosModels.js
+++ src/models/cursosModels.js
@@ -1,27 +1,27 @@
-const customerModel = {};
+const cursoModel = {};
 
-customerModel.getAll = (conn, callback) => {
+cursoModel.getAll = (conn, callback) => {
     conn.query('SELECT * FROM customer ORDER BY votos DESC', callback);
 };
 
-customerModel.insert = (conn, data, callback) => {
+cursoModel.insert = (conn, data, callback) => {
     conn.query('INSERT INTO customer SET ?', [data], callback);
 };
 
-customerModel.getById = (conn, id, callback) => {
+cursoModel.getById = (conn, id, callback) => {
     conn.query('SELECT * FROM customer WHERE id = ?', [id], callback);
 };
 
-customerModel.update = (conn, id, newData, callback) => {
+cursoModel.update = (conn, id, newData, callback) => {
     conn.query('UPDATE customer SET ? WHERE id = ?', [newData, id], callback);
 };
 
-customerModel.delete = (conn, id, callback) => {
+cursoModel.delete = (conn, id, callback) => {
     conn.query('DELETE FROM customer WHERE id = ?', [id], callback);
 };
 
-customerModel.incrementVotes = (conn, id, callback) => {
+cursoModel.incrementVotes = (conn, id, callback) => {
     conn.query('UPDATE customer SET votos = votos + 1 WHERE id = ?', [id]);
 };
 
-module.exports = customerModel;
+module.exports = cursoModel;
